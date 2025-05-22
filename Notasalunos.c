ad43cd87d0b2f18d0b05008d21ccbcde83ae8a0d
acirazul <gerziakelly@hotmail.com>
2025-05-22 19:16:14 +0000

    teste1

--- Notasalunos.c
+++ Notasalunos.c
@@ -2,17 +2,18 @@
 
 int main(){
     int nota1, nota2, nota3;
-    int media;
+    float media;
 
-    printf("*** Programa de Cálculo de Média ***\n);
+    printf("*** Programa de Cálculo de Média ***\n");
     printf("Digite a sua primeira nota: \n");
-    scanf("%d", &nota1);
+    scanf("%f", &nota1);
 
     printf("Digite a sua segunda nota: \n");
-    scanf("%d", &nota2);
+    scanf("%f", &nota2);
 
     printf("Digite a sua terceira nota: \n");
-    scanf("%d", &nota3;
+    scanf("%f", &nota3);
+
 
     media = (nota1 + nota2 + nota3) / 3;
 
